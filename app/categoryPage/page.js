7866254ad2481f1d77bc04140ede8248279d8f3a
fortune.akinremi <fortuneakinremi@gmail.com>
2025-03-14 00:33:35 -0400

    Added state to keep track of the current category in the category slider the user is. Also added more categories -- created an array of object, where each object is a category containing a name, image, and total amount of artworks in that category. Makes the display div dynamic.

---
+++ app/categoryPage/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+import { useState } from "react"
+
+
+export default function CategoryPage () {
+  // Do some stuff here
+
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -5,6 +5,50 @@ import Image from "next/image";
 import { ArrowLeftIcon, ArrowLongLeftIcon, ArrowRightIcon, ArrowUpTrayIcon } from "@heroicons/react/24/solid"
 
 export default function Home() {
+  /* State for the different art categories: */
+  const [currentCategory, setCurrentCategory] = useState(0);
+
+
+  /* Array of categories: */
+  const categories = [
+    {
+      image: "/abstract.jpg",
+      name: "ABSTRACT",
+      art_total: 2
+    },
+
+    {
+      image: "/minimal_1.jpg",
+      name: "MINIMALISM",
+      art_total: 3
+    },
+  ]
+
+
+  /* Function to move to prev category: */
+  const handlePrev = () => {
+    if (currentCategory > 0) {
+      setCurrentCategory(currentCategory - 1);
+    }
+    else if (currentCategory === 0) {
+      /* Wrap to the last category */
+      setCurrentCategory(categories.length - 1);
+    }
+  }
+
+
+  /* Function to move to next category: */
+  const handleNext = () => {
+    if (currentCategory < categories.length - 1) {
+      setCurrentCategory(currentCategory + 1);
+    }
+    else if (currentCategory === categories.length - 1) {
+      /* Wrap to the first category */
+      setCurrentCategory(0);
+    }
+  }
+
+
 
   return (
     /* Main container: */
@@ -32,43 +76,49 @@ export default function Home() {
           <h1 className="text-9xl font-medium -mt-4">Gallery</h1>
         </div>
 
+
         {/* Dividing Line: */}
         <div className="w-5/6 h-[1px] bg-black/40 mt-10"/>
+
 
         {/* Gallery: */}
         <div className="flex justify-between items-center boder-2 border-black w-5/6 h-full mt-10 gap-10">
           {/* Previous Arrow */}
-          <button className="flex justify-center items-center w-10 h-10 rounded-full border-black border-[1px] cursor-pointer">
-            <ArrowLeftIcon className="size-5"/>
+          <button className="flex justify-center items-center size-16 rounded-full border-black border-[1px] cursor-pointer"
+                  onClick={handlePrev}>
+            <ArrowLeftIcon className="size-7 hover:size-6"/>
           </button>
+
           {/* Gallery Images + Detail */}
-          <div className="w-2/3 h-60 sm:h-96">
+          <div className="w-2/3 h-60 sm:h-132 border-[1px] border-black">
             {/* Image */}
             <div className="w-full h-full overflow-hidden cursor-pointer">
               <Image
-                src={"/abstract_img.jpg"}
-                alt={"Abstract Art"}
+                src={categories[currentCategory].image}
+                alt={"Art Category"}
                 width={1920} height={1080}
-                className="w-fit h-fit hover:scale-105 transition-transform ease-in-out duration-1000"
+                className="w-fit h-fit hover:scale-105 transition-transform ease-in-out duration-700"
               />
             </div>
             {/* Details */}
-            <div className="flex flex-row justify-between gap-14 text-black/70 mt-1">
-              <p className="flex text-xs w-5/6 justify-start">Little description about the artwork we see above</p>
-              <p className="flex text-xs w-1/6 justify-end">Jan. 2019</p>
+            <div className="flex flex-col justify-between text-black/70 mt-3">
+              <p className="text-2xl md:text-4xl lg:text-6xl font-semibold justify-start">{categories[currentCategory].name}</p>
+              <p className="text-lg justify-start text-start">{categories[currentCategory].art_total} artworks</p>
             </div>
           </div>
+
           {/* Next Arrow: */}
-          <button className="flex justify-center items-center w-10 h-10 rounded-full border-black border-[1px] cursor-pointer">
-            <ArrowRightIcon className="size-5"/>
+          <button className="flex justify-center items-center size-16 rounded-full border-black border-[1px] cursor-pointer"
+                  onClick={handleNext}>
+            <ArrowRightIcon className="size-7 hover:size-6"/>
           </button>
         </div>
 
 
         {/* Footer: */}
-        <div className="flex flex-col w-full h-64 mt-20 bg-black text-neutral-100 items-center">
+        <div className="flex flex-col w-full h-64 mt-56 bg-black text-neutral-100 items-center">
           {/* CTA */}
-          <div className="flex flex-col items-center justify-center text-6xl font-semibold mt-5">
+          <div className="flex flex-col items-center justify-center text-6xl font-semibold mt-7">
             <p className="text-neutral-500 -mb-1">CREATE & SHARE</p>
             <p>YOUR ARTWORK</p>
           </div>
