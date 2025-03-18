b75c15819c760bcb9490559b666a05d08d2833b2
fortune.akinremi <fortuneakinremi@gmail.com>
2025-03-18 12:00:33 -0400

    Editted the footer

--- app/page.js
+++ app/page.js
@@ -120,20 +120,22 @@ export default function Home() {
 
 
         {/* Footer: */}
-        <div className="flex flex-col w-full h-64 mt-56 bg-black text-neutral-100 items-center">
+        {/* <div className="flex flex-col w-full h-64 mt-56 bg-black text-neutral-100 items-center"> */}
+        <div className="flex flex-col w-full h-10 mt-56 bg-black text-neutral-100 items-center justify-center">
           {/* CTA */}
-          <div className="flex flex-col items-center justify-center text-6xl font-semibold mt-7">
+          {/* <div className="flex flex-col items-center justify-center text-6xl font-semibold mt-7">
             <p className="text-neutral-500 -mb-1">CREATE & SHARE</p>
             <p>YOUR ARTWORK</p>
-          </div>
+          </div> */}
+
           {/* Button: */}
-          <button className="flex justify-center items-center mt-4 bg-neutral-200 
+          {/* <button className="flex justify-center items-center mt-4 bg-neutral-200 
                              w-fit text-black text-sm px-4 py-1 cursor-pointer">
             upload
             <ArrowUpTrayIcon className="ml-2 size-3"/>
-          </button>
+          </button> */}
           {/* Copyright stuff: */}
-          <p className="text-neutral-500 mt-10 text-xs">2025 Art Gallery. All rights reserved.</p>
+          <p className="flex text-neutral-500 text-xs">2025 Art Gallery. All rights reserved.</p>
         </div>
       </div>
     </div>
