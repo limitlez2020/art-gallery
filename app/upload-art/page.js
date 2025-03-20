c31228b230d26275a14300ab50e366455dc509b8
fortune.akinremi <fortuneakinremi@gmail.com>
2025-03-20 13:27:15 -0400

    Added message and state for when the artwork has been submitted - upload-art page

--- app/upload-art/page.js
+++ app/upload-art/page.js
@@ -9,6 +9,7 @@ import {Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigg
 import { db, storage } from "@/firebase.js"
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { time } from "framer-motion";
 
 export default function UploadArt () {
 
@@ -18,7 +19,8 @@ export default function UploadArt () {
   const [artCategory, setArtCategory] = useState("")
   const [artworkStory, setArtworkStory] = useState("")
   const [artworkImage, setArtworkImage] = useState(null)
-  const [uploading, setUploading] = useState(false) /* Track if form is submitting */
+  const [uploading, setUploading] = useState(false) /* Track if form is uploading to database */
+  const [submitted, setSubmitted] = useState(false) /* Track if form is submitted */
 
 
   /* Function to upload artwork image to the firebase database: */
@@ -61,14 +63,22 @@ export default function UploadArt () {
         artworkStory,
         // TODO: uncomment below
         // imageUrl,
+        timestamp: serverTimestamp()
       };
-  
-      // TODO: test - print the data
-      console.log(artworkData)
 
       /* Save the artwork data to the database */
       await addDoc(collection(db, "artworks"), artworkData);
-      console.log("Artwork added to database", artworkData);
+
+      /* Reset the states */
+      setArtistName("");
+      setArtworkName("");
+      setArtCategory("");
+      setArtworkStory("");
+      // TODO: uncomment below
+      // setArtworkImage(null);
+
+      /* Update submit state */
+      setSubmitted(true)
     }
     catch (error) {
       console.error("Error submitting artwork: ", error)
@@ -79,94 +89,105 @@ export default function UploadArt () {
   }
 
 
+
+
   return (
     <div className="flex flex-col h-full min-h-screen bg-neutral-200">
       <NavBar/>
       
       {/* Form Container */}
       <div className="flex my-20 w-[93%] justify-center items-center self-center">
-        {/* Form */}
-        <form className="flex flex-col w-[90%] border-[1px] items-center bg-white py-8"
-              onSubmit={handleSubmit}>
-          {/* Header: */}
-          <div className="flex flex-col items-center">
-            <p className="text-base font-medium">Upload Artwork</p>
-            <p
-            className="font-normal text-neutral-400 dark:text-neutral-400 text-xs text-center">
-            Drag or drop your files here or click to upload
-          </p>
+        {submitted ? (
+          /* Display message that the artwork has been submitted */
+          <div className="flex flex-grow h-full min-h-[69.7vh] bg-lack items-center justify-center">
+            ARTWORK has been submitted
           </div>
-
-          {/* Upload area */}
-          <div className="flex flex-col p-2 mt-5 w-5/6 items-center justify-center border-[1px] border-neutral-300 border-dotted">
-            <FileUpload onChange={(file) => setArtworkImage(file)}/>
-          </div>
-
-          {/* Artist and artwork details: */}
-          <div className="flex flex-col mt-5 w-5/6 gap-4">
-            {/* Artist Name: */}
-            <div className="flex flex-col w-full">
-              <p className="text-xs">Name</p>
-              <input
-                required 
-                type="text" 
-                className="w-full border-[1px] border-neutral-200 text-xs p-2"
-                placeholder="Type your name"
-                value={artistName}
-                onChange={(e) => {setArtistName(e.target.value)}}
-              />
+        ) : (
+          /* Form */
+          <form className="flex flex-col w-[90%] border-[1px] items-center bg-white py-8"
+                onSubmit={handleSubmit}>
+            {/* Header: */}
+            <div className="flex flex-col items-center">
+              <p className="text-base font-medium">Upload Artwork</p>
+              <p
+              className="font-normal text-neutral-400 dark:text-neutral-400 text-xs text-center">
+              Drag or drop your files here or click to upload
+            </p>
             </div>
 
-            {/* Artwork Name: */}
-            <div className="flex flex-col w-full">
-              <p className="text-xs">Artwork Name</p>
-              <input
-                required 
-                type="text" 
-                className="w-full border-[1px] border-neutral-200 text-xs p-2"
-                placeholder="Type the name of your artwork"
-                value={artworkName}
-                onChange={(e) => {setArtworkName(e.target.value)}}
-              />
+            {/* Upload area */}
+            <div className="flex flex-col p-2 mt-5 w-5/6 items-center justify-center border-[1px] border-neutral-300 border-dotted">
+              <FileUpload onChange={(file) => setArtworkImage(file)}/>
             </div>
 
-            {/* Artwork Category: */}
-            <div className="flex flex-col w-full">
-              <p className="text-xs">Artwork Category</p>
-              <Select onValueChange={setArtCategory}>
-                <SelectTrigger className="w-full rounded-none text-xs">
-                  <SelectValue placeholder="Select an art category" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Category</SelectLabel>
-                    <SelectItem value="abstract" className="text-sm">Abstract</SelectItem>
-                    <SelectItem value="minimalism" className="text-sm">Minimalism</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
+            {/* Artist and artwork details: */}
+            <div className="flex flex-col mt-5 w-5/6 gap-4">
+              {/* Artist Name: */}
+              <div className="flex flex-col w-full">
+                <p className="text-xs">Name</p>
+                <input
+                  required 
+                  type="text" 
+                  className="w-full border-[1px] border-neutral-200 text-xs p-2"
+                  placeholder="Type your name"
+                  value={artistName}
+                  onChange={(e) => {setArtistName(e.target.value)}}
+                />
+              </div>
+
+              {/* Artwork Name: */}
+              <div className="flex flex-col w-full">
+                <p className="text-xs">Artwork Name</p>
+                <input
+                  required 
+                  type="text" 
+                  className="w-full border-[1px] border-neutral-200 text-xs p-2"
+                  placeholder="Type the name of your artwork"
+                  value={artworkName}
+                  onChange={(e) => {setArtworkName(e.target.value)}}
+                />
+              </div>
+
+              {/* Artwork Category: */}
+              <div className="flex flex-col w-full">
+                <p className="text-xs">Artwork Category</p>
+                <Select onValueChange={setArtCategory}>
+                  <SelectTrigger className="w-full rounded-none text-xs">
+                    <SelectValue placeholder="Select an art category" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Category</SelectLabel>
+                      <SelectItem value="abstract" className="text-sm">Abstract</SelectItem>
+                      <SelectItem value="minimalism" className="text-sm">Minimalism</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+
+
+              {/* Story behind artwork */}
+              <div className="flex flex-col w-full">
+                <p className="text-xs">Artwork Story</p>
+                <textarea
+                  required
+                  className="text-w-full border-[1px] h-20 border-neutral-200 text-xs p-2"
+                  placeholder="Story behind your artwork"
+                  value={artworkStory}
+                  onChange={(e) => setArtworkStory(e.target.value)}
+                />
+              </div>
             </div>
 
+            {/* Submit the form */}
+            <button className="flex self-center justify-center bg-black mt-4 p-2 px-10 text-white text-xs cursor-pointer">
+              {uploading ? "Uploading..." : "Submit"}
+            </button>
 
-            {/* Story behind artwork */}
-            <div className="flex flex-col w-full">
-              <p className="text-xs">Artwork Story</p>
-              <textarea
-                required
-                className="text-w-full border-[1px] h-20 border-neutral-200 text-xs p-2"
-                placeholder="Story behind your artwork"
-                value={artworkStory}
-                onChange={(e) => setArtworkStory(e.target.value)}
-              />
-            </div>
-          </div>
+          </form>
+        )}
 
-          {/* Submit the form */}
-          <button className="flex self-center justify-center bg-black mt-4 p-2 px-10 text-white text-xs cursor-pointer">
-            Submit
-          </button>
-
-        </form>
+      
       </div>
 
       <Footer/>
